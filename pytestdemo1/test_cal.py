4bf2f5e1ef8d5c0f03ed775e584bd4a5aeccb4b5
ckk-sys <1224174298@qq.com>
2021-03-29 22:51:18 +0800

    pytest test2 allure测试报告

--- pytestdemo1/test_cal.py
+++ pytestdemo1/test_cal.py
@@ -3,25 +3,30 @@ import allure
 
 
 @allure.feature("计算器功能测试")
+@allure.title("计算器功能测试")
 class TestCal:
 
     # 测试相加功能
     @allure.story("相加功能")
+    @allure.title("相加功能")
     def test_add(self, get_calc, get_add_datas):
         assert round(get_calc.add(get_add_datas[0], get_add_datas[1])) == get_add_datas[2]
 
     # 测试相减功能
     @allure.story("相减功能")
+    @allure.title("相减功能")
     def test_sub(self, get_calc, get_sub_datas):
         assert get_calc.sub(get_sub_datas[0], get_sub_datas[1]) == get_sub_datas[2]
 
     # 测试相乘功能
     @allure.story("相乘功能")
+    @allure.title("相乘功能")
     def test_mul(self, get_calc, get_mul_datas):
         assert get_calc.mul(get_mul_datas[0], get_mul_datas[1]) == get_mul_datas[2]
 
     # 测试相除功能
     @allure.story("相除功能")
+    @allure.title("相除功能")
     def test_div(self, get_calc, get_div_datas):
         try:
             assert get_calc.div(get_div_datas[0], get_div_datas[1]) == get_div_datas[2]
